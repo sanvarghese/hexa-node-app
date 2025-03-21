085f8b059726f153b9436135ce0a993fe3989c27
sanvarghese <sanjose0023@gmail.com>
2025-03-21 16:11:28 +0530

    feat: add banner and nav

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Home from "./pages/home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+	return (
+		<>
+			<Header />
+			<Home />
+			<Footer />
+		</>
+	);
+}
+
+export default App;
